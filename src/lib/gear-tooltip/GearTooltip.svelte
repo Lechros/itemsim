c911a7855661e4ac554617ce8f32df643b92e428
OniOniOn- <0onionion0@gmail.com>
2023-03-22 15:52:50 +0900

    fix: fix error on undefined potential option

--- src/lib/gear-tooltip/GearTooltip.svelte
+++ src/lib/gear-tooltip/GearTooltip.svelte
@@ -199,6 +199,9 @@
 						upgradeFailCount={gear.upgradeFailCount}
 					/>
 				{/if}
+				{#if gear.hammerCount > 0}
+					<div class="text white">황금망치 제련 적용</div>
+				{/if}
 				{#if gear.karma !== undefined}
 					<div class="text orange2">가위 사용 가능 횟수 : {gear.karma}회</div>
 				{/if}
@@ -207,9 +210,6 @@
 				{/if}
 				{#if gear.getBooleanValue(GearPropType.fixedPotential)}
 					<div class="text white">에디셔널 잠재능력 설정 불가</div>
-				{/if}
-				{#if gear.hammerCount > 0}
-					<div class="text white">황금망치 제련 적용</div>
 				{/if}
 				{#if superior}
 					<div class="text green">{getGearPropString(GearPropType.superiorEqp, 1)}</div>
