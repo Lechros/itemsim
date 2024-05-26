37f99ab0cdcba188e291d037b7f893affffd1510
Lechros <0onionion0@gmail.com>
2024-05-26 19:47:47 +0900

    feat: update exceptional parts data

--- src/lib/enchant/domains/exceptional.ts
+++ src/lib/enchant/domains/exceptional.ts
@@ -30,6 +30,10 @@ export function getExceptionalParts(gear: Gear) {
 			return [exceptionalParts['악몽의 조각']];
 		case 1012632:
 			return [exceptionalParts['그라비티 모듈']];
+		case 1022278:
+			return [exceptionalParts['파멸의 징표']];
+		case 1032316:
+			return [exceptionalParts['충정의 투구']];
 	}
 	return [];
 }
@@ -61,6 +65,34 @@ const exceptionalParts = {
 			[GearPropType.incMAD, 10],
 			[GearPropType.incMHP, 750],
 			[GearPropType.incMMP, 750]
+		])
+	},
+	'파멸의 징표': {
+		name: '파멸의 징표',
+		icon: 2644202,
+		option: new Map([
+			[GearPropType.incSTR, 15],
+			[GearPropType.incDEX, 15],
+			[GearPropType.incINT, 15],
+			[GearPropType.incLUK, 15],
+			[GearPropType.incPAD, 10],
+			[GearPropType.incMAD, 10],
+			[GearPropType.incMHP, 750],
+			[GearPropType.incMMP, 750]
+		])
+	},
+	'충정의 투구': {
+		name: '충정의 투구',
+		icon: 2644203,
+		option: new Map([
+			[GearPropType.incSTR, 20],
+			[GearPropType.incDEX, 20],
+			[GearPropType.incINT, 20],
+			[GearPropType.incLUK, 20],
+			[GearPropType.incPAD, 15],
+			[GearPropType.incMAD, 15],
+			[GearPropType.incMHP, 1000],
+			[GearPropType.incMMP, 1000]
 		])
 	}
 };
