72b17c600dc3215686e8e0e7f79a5fd3580ee724
Lechros <0onionion0@gmail.com>
2025-03-02 17:47:43 +0900

    feat: add OriginIcon component

---
+++ src/shared/ui/OriginIcon.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let {
+		src,
+		alt,
+		origin,
+		scale = 1
+	}: {
+		src: string;
+		alt: string;
+		origin?: [number, number];
+		scale?: number;
+	} = $props();
+</script>
+
+<div class="h-8 w-8" style="scale: {scale}">
+	{#if origin}
+		<img {src} {alt} style="margin-left: {-origin[0]}px; margin-top: {32 - origin[1]}px" />
+	{/if}
+</div>
+
+<style>
+	img {
+		image-rendering: pixelated;
+	}
+</style>
---
+++ src/shared/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as OriginIcon } from './OriginIcon.svelte';
