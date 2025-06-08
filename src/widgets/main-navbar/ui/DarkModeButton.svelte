c566f2180573ce54760ace8af731750abb00b9e5
Lechros <0onionion0@gmail.com>
2025-06-09 07:03:44 +0900

    fix: 다크 모드 버튼 애니메이션 추가

--- src/widgets/main-navbar/ui/DarkModeButton.svelte
+++ src/widgets/main-navbar/ui/DarkModeButton.svelte
@@ -1,15 +1,24 @@
 <script>
 	import { Button } from '$lib/shared/shadcn/components/ui/button';
+	import { cn } from '$lib/shared/shadcn/utils';
 	import { MoonIcon, SunIcon } from 'lucide-svelte';
-	import { toggleMode } from 'mode-watcher';
+	import { toggleMode, mode } from 'mode-watcher';
+
+	const dark = $derived(mode.current === 'dark');
 </script>
 
 <Button onclick={toggleMode} variant="ghost" size="icon">
 	<SunIcon
-		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+		class={cn(
+			'h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all',
+			dark && 'scale-0 -rotate-90'
+		)}
 	/>
 	<MoonIcon
-		class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+		class={cn(
+			'absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all',
+			dark && 'scale-100 rotate-0'
+		)}
 	/>
 	<span class="sr-only">Toggle theme</span>
 </Button>
