5b2cfc651fa11f96bbac7c7a0456078163b678ad
Lechros <0onionion0@gmail.com>
2025-06-09 01:23:26 +0900

    chore: 사용되지 않는 폰트 제거

--- src/entities/gear-tooltip2/ui/parts/TemplateText.svelte
+++ src/entities/gear-tooltip2/ui/parts/TemplateText.svelte
@@ -65,31 +65,3 @@
 		{/each}
 	{/each}
 </div>
-
-<style>
-	.text {
-		white-space: pre-wrap;
-		font-family: 'Morris';
-		font-size: 12px;
-		line-height: 16px;
-		color: #ffffff;
-	}
-	.text :global(.emphasis) {
-		color: #ffcc00;
-	}
-	.text :global(.red) {
-		color: #ff6633;
-	}
-	.text :global(.gray) {
-		color: #b7bfc5;
-	}
-	.text :global(.starforce) {
-		color: #ffcc00;
-	}
-	.text :global(.scroll) {
-		color: #afadff;
-	}
-	.text :global(.bonusStat) {
-		color: #0ae3ad;
-	}
-</style>
