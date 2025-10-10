4bb7e15ea1d3a87b4add52560a53376f1fe96fc5
Lechros <0onionion0@gmail.com>
2025-10-11 05:42:58 +0900

    feat; 모바일 버튼 UI 통일 및 이미지 경고 디자인 개선

--- src/lib/features/gear-enchant-ui/tabs/manage/components/ImageDialog.svelte
+++ src/lib/features/gear-enchant-ui/tabs/manage/components/ImageDialog.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import { Alert, AlertDescription, AlertTitle } from '$lib/components/ui/alert';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import {
 		Dialog,
 		DialogContent,
 		DialogFooter,
 		DialogHeader,
+		DialogTitle,
 		DialogTrigger
 	} from '$lib/components/ui/dialog';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 	import { GearTooltipRenderer } from '$lib/features/gear-tooltip-renderer';
 	import { ReadonlyGear } from '@malib/gear';
 	import { toPng } from 'html-to-image';
-	import { Loader2 } from 'lucide-svelte';
+	import { AlertCircle, Loader2 } from 'lucide-svelte';
 	import { untrack } from 'svelte';
 	import { devicePixelRatio } from 'svelte/reactivity/window';
 	let {
@@ -58,35 +60,41 @@
 			link.click();
 		}
 	};
+
+	const close = () => {
+		open = false;
+	};
 </script>
 
 <Dialog bind:open {onOpenChangeComplete}>
 	<DialogTrigger class={buttonVariants()}>열기</DialogTrigger>
-	<DialogContent class="w-fit">
-		<DialogHeader />
+	<DialogContent class="max-h-full w-fit max-w-none p-4 pt-4">
+		<DialogHeader>
+			<DialogTitle class="text-base">아이템 이미지</DialogTitle>
+		</DialogHeader>
+
 		{#if dataUrl}
-			<div>
+			<div class="flex flex-col gap-2">
 				<img src={dataUrl} alt={gear.name} class="max-w-none" style="width: 324px" />
 				{#if maybeInvalidImage}
-					<p class="mt-2 text-sm" style="max-width: 324px">
-						이미지가 정확하지 않을 수 있어요.<br />
-						PC와 브라우저의 배율이 100%인지 확인해 주세요.
-					</p>
+					<Alert variant="destructive">
+						<AlertCircle />
+						<AlertTitle>이미지가 정확하지 않을 수 있어요.</AlertTitle>
+						<AlertDescription>PC와 브라우저의 배율이 100%인지 확인해 주세요.</AlertDescription>
+					</Alert>
 				{/if}
 			</div>
 		{:else}
 			<div class="relative">
 				<div bind:this={container}>
-					<GearTooltipRenderer
-						{gear}
-						incline={{ combat: 0 }}
-					/>
+					<GearTooltipRenderer {gear} incline={{ combat: 0 }} />
 				</div>
 				<Skeleton class="absolute inset-0" />
 			</div>
 		{/if}
-		<DialogFooter>
-			<Button disabled={!dataUrl} onclick={download}>
+		<DialogFooter class="flex-row justify-end">
+			<Button variant="outline" class="flex-1/3 sm:flex-none" onclick={close}>닫기</Button>
+			<Button class="flex-2/3 sm:flex-none" disabled={!dataUrl} onclick={download}>
 				{#if !dataUrl}
 					<Loader2 class="animate-spin" />
 				{/if}
--- src/lib/features/gear-inventory/dialog/GearDialog.svelte
+++ src/lib/features/gear-inventory/dialog/GearDialog.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-	import { GearTooltipRenderer } from '$lib/features/gear-tooltip-renderer';
 	import { Button } from '$lib/components/ui/button';
-	import { Dialog, DialogContent, DialogFooter, DialogHeader } from '$lib/components/ui/dialog';
+	import {
+		Dialog,
+		DialogContent,
+		DialogFooter,
+		DialogHeader,
+		DialogTitle
+	} from '$lib/components/ui/dialog';
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import { GearTooltipRenderer } from '$lib/features/gear-tooltip-renderer';
 	import { ReadonlyGear } from '@malib/gear';
 
 	let {
@@ -18,16 +25,20 @@
 </script>
 
 <Dialog bind:open>
-	<DialogContent class="max-h-[calc(100%-2rem)] w-fit max-w-none overflow-y-auto">
-		<DialogHeader />
+	<DialogContent class="max-h-full w-fit max-w-none p-4 pt-6">
+		<DialogHeader>
+			<DialogTitle />
+		</DialogHeader>
 
 		<div class="flex justify-center">
-			<GearTooltipRenderer {gear} incline={{ combat: 0 }} />
+			<ScrollArea class="max-h-[calc(100dvh-8rem)]">
+				<GearTooltipRenderer {gear} incline={{ combat: 0 }} />
+			</ScrollArea>
 		</div>
 
-		<DialogFooter>
-			<Button variant="outline" onclick={onClose}>닫기</Button>
-			<Button onclick={onAccept}>강화하기</Button>
+		<DialogFooter class="flex-row justify-end">
+			<Button variant="outline" class="flex-1/3 sm:flex-none" onclick={onClose}>닫기</Button>
+			<Button class="flex-2/3 sm:flex-none" onclick={onAccept}>강화하기</Button>
 		</DialogFooter>
 	</DialogContent>
 </Dialog>
