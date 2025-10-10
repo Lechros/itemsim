bc49c8ca622d16afb32ba74639fb4512fa609df4
Lechros <0onionion0@gmail.com>
2025-10-10 19:07:01 +0900

    style: reformat

--- src/lib/stores/exclusive-equip.svelte.ts
+++ src/lib/stores/exclusive-equip.svelte.ts
@@ -1,13 +1,13 @@
 import type { ExclusiveEquipData } from '$lib/api';
 
 export function createExclusiveEquipsLoader(data?: ExclusiveEquipData) {
-    const exclusiveEquipMap = new Map<number, string[]>();
-    for ( const exclusiveEquip of Object.values(data ?? {})) {
-        for ( const itemId of exclusiveEquip.itemIds) {
-            exclusiveEquipMap.set(itemId, exclusiveEquip.names);
-        }
-    }
-    
+	const exclusiveEquipMap = new Map<number, string[]>();
+	for (const exclusiveEquip of Object.values(data ?? {})) {
+		for (const itemId of exclusiveEquip.itemIds) {
+			exclusiveEquipMap.set(itemId, exclusiveEquip.names);
+		}
+	}
+
 	return (gearId: number): string[] => {
 		return exclusiveEquipMap.get(gearId) || [];
 	};
