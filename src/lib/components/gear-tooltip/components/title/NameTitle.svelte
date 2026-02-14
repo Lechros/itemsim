d4fd3d9a308b7418e2cb4000ab280b92458f05ca
Lechros <0onionion0@gmail.com>
2026-02-15 01:06:36 +0900

    feat: 구버전 툴팁을 추후 적용할 수 있도록 코드 업데이트

--- src/lib/components/gear-tooltip/components/title/NameTitle.svelte
+++ src/lib/components/gear-tooltip/components/title/NameTitle.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+	import { GearGender } from '@malib/gear';
+
 	let {
 		name,
+		gender,
 		scrollUpgradeCount,
 		color
-	}: { name: string; scrollUpgradeCount: number; color: string } = $props();
+	}: { name: string; gender: GearGender | undefined; scrollUpgradeCount: number; color: string } =
+		$props();
 </script>
 
 <div class="gt--title gt--{color}">
 	{name}
+	{#if gender === GearGender.Male}
+		(남)
+	{:else if gender === GearGender.Female}
+		(여)
+	{/if}
 	{#if scrollUpgradeCount > 0}
 		(+{scrollUpgradeCount})
 	{/if}
--- src/lib/features/gear-tooltip-renderer/GearTooltipRenderer.svelte
+++ src/lib/features/gear-tooltip-renderer/GearTooltipRenderer.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
-	import { GearTooltip } from '$lib/components/gear-tooltip2';
+	import { GearTooltip } from '$lib/components/gear-tooltip';
+	import { GearTooltip as GearTooltip2 } from '$lib/components/gear-tooltip2';
 	import type { ReadonlyGear } from '@malib/gear';
 	import { createExclusiveEquipsLoader, useExclusiveEquips } from './exclusive-equip';
 	import { createSetItemNameLoader, useSetItems } from './set-item';
-	import type { ComponentProps } from 'svelte';
+
+	type Tooltip1Props = {
+		cannot?: {
+			level?: boolean;
+			job?: boolean;
+			gender?: boolean;
+		};
+		incline: { combat: number } | { attack: number } | { attack: number; combat: number };
+	};
+	type Tooltip2Props = {
+		cannot?: {
+			level?: boolean;
+			job?: boolean;
+			gender?: boolean;
+		};
+		incline: { equipped: true } | { combat: number };
+		expand?: boolean;
+	};
 
 	let {
 		gear,
+		tooltipVersion = '2',
 		...props
-	}: {
-		gear: ReadonlyGear;
-	} & Omit<ComponentProps<typeof GearTooltip>, 'loadSetItemName' | 'loadExclusiveEquips'> =
-		$props();
+	}: { gear: ReadonlyGear; tooltipVersion: '1' | '2' } & (Tooltip1Props | Tooltip2Props) = $props();
 
 	const setItems = useSetItems();
 	const exclusiveEquips = useExclusiveEquips();
@@ -20,4 +36,8 @@
 	const loadExclusiveEquips = $derived(createExclusiveEquipsLoader(exclusiveEquips.data));
 </script>
 
-<GearTooltip {gear} {loadSetItemName} {loadExclusiveEquips} {...props} />
+{#if tooltipVersion === '1'}
+	<GearTooltip {gear} {loadExclusiveEquips} {...props as Tooltip1Props} />
+{:else if tooltipVersion === '2'}
+	<GearTooltip2 {gear} {loadSetItemName} {loadExclusiveEquips} {...props as Tooltip2Props} />
+{/if}
