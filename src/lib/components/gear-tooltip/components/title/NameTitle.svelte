fc711403caee25d3aecf12e32402e4175d640ed5
Lechros <0onionion0@gmail.com>
2026-02-15 15:54:28 +0900

    fix: 구버전 장비 툴팁 제목이 잘못 정렬되던 오류 수정

--- src/lib/components/gear-tooltip/components/title/NameTitle.svelte
+++ src/lib/components/gear-tooltip/components/title/NameTitle.svelte
@@ -6,18 +6,27 @@
 		gender,
 		scrollUpgradeCount,
 		color
-	}: { name: string; gender: GearGender | undefined; scrollUpgradeCount: number; color: string } =
-		$props();
+	}: {
+		name: string;
+		gender: GearGender | undefined;
+		scrollUpgradeCount: number;
+		color: string;
+	} = $props();
+
+	function getNameText(name: string, gender: GearGender | undefined, scrollUpgradeCount: number) {
+		let text = name;
+		if (gender === GearGender.Male) {
+			text += ' (남)';
+		} else if (gender === GearGender.Female) {
+			text += ' (여)';
+		}
+		if (scrollUpgradeCount > 0) {
+			text += ` (+${scrollUpgradeCount})`;
+		}
+		return text;
+	}
 </script>
 
 <div class="gt--title gt--{color}">
-	{name}
-	{#if gender === GearGender.Male}
-		(남)
-	{:else if gender === GearGender.Female}
-		(여)
-	{/if}
-	{#if scrollUpgradeCount > 0}
-		(+{scrollUpgradeCount})
-	{/if}
+	{getNameText(name, gender, scrollUpgradeCount)}
 </div>
