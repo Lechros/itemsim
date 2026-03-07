1ac8503d060a7bb5133103e205ac1d1c0c1d5883
Lechros <0onionion0@gmail.com>
2026-03-07 15:15:22 +0900

    feat: 사이트 아이콘 적용

--- src/lib/features/navigation/main-navbar/MainNavbar.svelte
+++ src/lib/features/navigation/main-navbar/MainNavbar.svelte
@@ -9,8 +9,11 @@
 	<div class="mx-auto flex h-full w-full px-2">
 		<div class="flex items-center gap-2">
 			<Button variant="ghost" href="/">
-				<img src={Logo} alt="logo" class="size-5" />
-				<span class="font-semibold"> 아이템 시뮬레이터 </span>
+				<div class="flex size-5 items-center justify-center">
+					<img src={Logo} alt="logo" class="size-8" />
+				</div>
+
+				아이템 시뮬레이터
 			</Button>
 		</div>
 		<div class="ml-auto flex items-center gap-1 px-4">
@@ -22,3 +25,10 @@
 		</div>
 	</div>
 </header>
+
+<style>
+	img {
+		max-width: none;
+		pointer-events: none;
+	}
+</style>
